67ec11def994558ab295da38a46be51b50030b3d
Francisco Molina <00300917@uca.edu.sv>
2018-06-21 16:42:53 -0600

    test

---
+++ Documents/NetBeansProjects/SlugTest/data/back.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="back" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="back.png" width="512" height="512"/>
+</tileset>
