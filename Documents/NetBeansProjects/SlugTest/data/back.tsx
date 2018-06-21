4082b1bba101a9f801d837dc5de3481b7a95e883
Francisco Molina <00300917@uca.edu.sv>
2018-06-21 17:10:52 -0600

    lol

--- Documents/NetBeansProjects/SlugTest/data/back.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="back" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="back.png" width="512" height="512"/>
-</tileset>
--- Documents/NetBeansProjects/SlugTest/data/rocks.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="rocks" tilewidth="32" tileheight="32" tilecount="16" columns="4">
- <properties>
-  <property name="block" type="bool" value="true"/>
- </properties>
- <image source="rocks.png" width="128" height="128"/>
-</tileset>
