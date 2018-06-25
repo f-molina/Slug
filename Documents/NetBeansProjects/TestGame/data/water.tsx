40f06884da528264f1604de4be07ed8b4d249985
Francisco Molina <00300917@uca.edu.sv>
2018-06-24 18:59:45 -0600

    recursos

---
+++ Documents/NetBeansProjects/TestGame/data/water.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="water" tilewidth="32" tileheight="32" tilecount="18" columns="3">
+ <image source="water.png" width="96" height="192"/>
+ <tile id="15">
+  <animation>
+   <frame tileid="15" duration="100"/>
+   <frame tileid="16" duration="100"/>
+   <frame tileid="17" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
